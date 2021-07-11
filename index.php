5a007faff1a40a6d0178ac1a6d1144be9676455b
ILC <76262541+Ilc-Ilia-Developer@users.noreply.github.com>
2021-07-11 20:58:08 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-
+<html>
+<head>
+	<title>PowerMC - The Best Free Minecraft Server Hosting</title>
+	<script src="./include.js"></script>
+	<style>
+	body {
+		margin: 0px;
+	}
+	.topnav{
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 2;
+		width: 100%;
+		height: 5%;
+		background-color: lightseagreen;
+		box-shadow: 0 8px 16px 0 rgba(32, 178, 170, 0.6), 0 12px 40px 0 rgba(32, 178, 170, 0.3);
+	}
+	.main{
+		width: 100%;
+		height: 100%;
+		background-color: darkseagreen;
+	}
+	</style>
+</head>
+<body>
+	<div class="topnav">
+		
+	</div>
+	<div class="main">
+		
+	</div>
+</body>
+</html>
